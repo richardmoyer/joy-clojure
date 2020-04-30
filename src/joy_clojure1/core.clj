7729fe44ab40ddb52202875c966f6fadfc9b6560
richm <richmoyer@gmail.com>
2020-04-29 22:33:58 -0500

    linfix

--- src/joy_clojure1/core.clj
+++ src/joy_clojure1/core.clj
@@ -1,6 +1,8 @@
 (ns joy-clojure1.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn r->lfix
+  ([a op b] (op a b))
+  ([a op1 b op2 c] (op1 a (op2 b c)))
+  ([a op1 b op2 c op3 d] (op1 a (op2 b (op3 c d)))))
+
+(r->lfix 1 + 2)
